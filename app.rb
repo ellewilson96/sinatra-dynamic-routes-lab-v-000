7011c73bb1e10737e89fb887988dd35f16b28bcf
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-07 00:50:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,7 +16,8 @@ class App < Sinatra::Base
    print params[:phrase] * params[:number].to_i
  end
 
- get '/say/:word1/:word2/:word3/:word4/:word5'
+ get '/say/:word1/:word2/:word3/:word4/:word5' do
  print params[:word1] + params[:word2] + params[:word3]
  + params[:word4] + params[:word5]
+ end
 end
