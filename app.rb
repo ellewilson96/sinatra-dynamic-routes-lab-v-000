af8ccf5b7f56f4e7ff72af5a618ad61b85552367
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-07 00:44:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+ get '/reversename/:name' do
+   @name = params[:name]
+   "#{@name.reverse}"
+ end
 
+ get '/square/:number' do
+   @square = params[:number].to_i
+   "#{@square * @square}"
+ end
+
+ get '/say/:number/:phrase' do
+   @phrase = params[:phrase]
+   print "#{@phrase + " "}" * params[:number].to_i
+ end
 end
